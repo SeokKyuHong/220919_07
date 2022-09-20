db4b21fbb8e1bd8bcb57cb62124cdaa9c7059c0e
HongKyu <105551449+SeokKyuHong@users.noreply.github.com>
2022-09-20 22:06:09 +0900

    홍석규_22시05분

--- app.py
+++ app.py
@@ -1,39 +1,70 @@
-from crypt import methods
-from turtle import title
-from flask import Flask, render_template
+
+from os import lseek
+from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
-import datetime as dt
-import time
 
 client = MongoClient('mongodb://SecondWind:clsgowlrlfqkfo@13.125.11.60', 27017)
 db = client.db_00_07
 
 app = Flask(__name__)
 
+# 회원가입 페이지 불러오기
 @app.route('/signup', methods=['GET'])
 def signup_read() :
     return render_template("signup.html")
 
-
+# 회원가입 데이터 전송
 @app.route('/signup', methods=['POST'])
 def signup_post() :
-    return render_template("signup.html")
 
+    url_id = request.form['url_id'] 
+    url_pw = request.form['url_pw'] 
+    url_pw2 = request.form['url_pw2']
+    url_name = request.form['url_name'] 
 
-#Read Board
-@app.route('/board', methods=['GET'])
-def read_board():
-    boards = list(db.boards.find({},{'_id':False}))
-    return render_template("board/board.html", boards=boards)
+    article = {'id': url_id, 'pw': url_pw, 'name': url_name}
+    
+    db.user.insert_one(article)
+    return jsonify({'result': 'success'})
 
+    # id_list = list(db.user.find({},{'_id':0, }))
+    # id_list2 = db.user.find_one({'id': ''})
+    # print(id_list)
+    # id_list_len = len(id_list)
+    # print(id_list_len)
 
-#Create Board
-@app.route('/createBoard', methods=['GET'])
-def create_board():
+# 회원가입 성공하면 로그인 페이지로 보내줌
+@app.route('/index', methods=['GET'])
+def main_read() :
+    return render_template("index.html")
+
+# 실시간 아이디 확인
+@app.route('/id_check', methods=['POST'])
+def id_check() :
+    url_id = request.form['url_id']
+    id_list = list(db.user.find({},{'_id':0, }))
+    # id_list2 = db.user.find_one({'id': ''})
+    # print(id_list)
+    id_list_len = len(id_list)
+    # print(id_list_len)
+    
+    for i in range(id_list_len):
+        
+        id_se = id_list[i]['id']
+        if id_se == url_id:
+            return jsonify({'result': 'success'})
+            
+        else:
+            print()
+    
+    return jsonify({'result': 'bb'})       
+        
+            
     
     
-    return render_template("board/createBoard.html")
+
+
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=4000,debug=True)
+   app.run('0.0.0.0',port=5001,debug=True)
--- static/signup.js
+++ static/signup.js
@@ -1,18 +1,67 @@
+
+// 회원가입 버튼 함수
 function post_signup() {
     const id = $("#post-signup-id").val();
-    const pw = $("#post-signup-pw").val();
+    const pass = $("#post-signup-pw1").val();
+    const pass2 = $("#post-signup-pw2").val();
+    const name = $("#post-signup-name").val();
 
     $.ajax({
         type: "POST",
-        url: "/memo",
-        data: {url_give: url, comment_give: comment},
+        url: "/signup",
+        data: {url_id: id, url_pw: pass, url_pw2: pass2, url_name: name},
+        success: function (response) { 
+            if (response["result"] == "success") {
+                
+                alert("회원가입완료! 로그인을 해주시기 바랍니다.");
+                location.href = 'index';
+                
+            }else if(response["result"] == "bb"){
+                alert("이미 등록한 아이디 입니다.")
+                window.location.reload(); //실패 했을떄 페이지 리로드
+            }else{
+                alert("서버오류!")
+            }
+            
+        }
+    })
+
+}
+
+//아이디 실시간 중복체크
+function checkId() {
+    const id = $("#post-signup-id").val();
+
+    $.ajax({
+        type: "POST",
+        url: "/id_check",
+        data: {url_id: id},
         success: function (response) { // 성공하면
             if (response["result"] == "success") {
-                alert("포스팅 성공했어");
-                window.location.reload(); //성공했을떄 페이지 리로드
+                $('#checkid').html("사용불가능한 아이디 입니다.")
+                $('#checkid').css('color', 'red')
+                
+            }else if(response["result"] == "bb"){
+                
+                $('#checkid').html("사용가능한 아이디 입니다.")
+                $('#checkid').css('color', 'blue')
+                
             }else{
-                alert("서버오류!!!!!!!")
+                alert("서버오류!")
             }
         }
     })
+
+}
+
+
+//실시간 비밀번호 중복 체크
+function passwordCheckFunction(){
+    var password1 = $("#post-signup-pw1").val();
+    var password2 = $("#post-signup-pw2").val();
+    if(password1 != password2){
+        $("#checkMessage").html("비밀번호가 일치하지 않습니다.");
+    } else {
+        $("#checkMessage").html("");
+    }
 }
